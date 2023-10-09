5adf05d832e6ddfbdda472e30da39f3179a091bb
Augusto Silva <acesardasilva@coursedog.com>
2023-10-09 01:39:27 -0300

    chore: attempt to create a conflict when merging

---
+++ tests/google_translate_test.py
@@ -0,0 +1,8 @@
+changeg file to generate a new conflict
+
+("Lorem ipsum dolor sit amet, consectetur adipis"
+ "cing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu"
+ "a. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
+ "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum "
+ "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
+ "sunt in culpa qui officia deserunt mollit anim id est laborum.")
